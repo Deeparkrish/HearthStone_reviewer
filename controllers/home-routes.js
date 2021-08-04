6f46da8f943488a7e3d750d377e17143ffd77be6
Deepa <kosalai17@gmail.com>
2021-08-04 16:47:09 -0700

    log in page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,14 @@
+const router = require('express').Router();
+// Home page 
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
+// Login Page 
+// login  api routes 
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+module.exports = router;
 // To post comments on the page 
 
-// login  api routes 
+
