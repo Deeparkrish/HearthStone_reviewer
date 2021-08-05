c16848ed74ed2f9fefa0709d42459f17fcfa425d
JessicaAKelley <kelleyajessica@gmail.com>
2021-08-05 17:06:21 -0500

    Models and associations with Yorn

---
+++ models/Cards.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+Cards.init(
+    {
+        cardId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true 
+        },
+        // Need ID from API
+        apiId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        }
+},
+{
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'card'
+    }
+);
+
+module.exports = Cards;
--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
-// import all models
+// Import all models
+const Cards = require('./Cards');
+const Comment = require('./Comment');
 const User = require('./User');
 
+// Create Associations 
+Comment.oneToMany(ForeignKeyToCard);
 
+Cards.hasOne(Comment);
 
+User.
 
-//Create Associations 
 
 
 
