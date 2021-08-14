4ed0f5ec7a3e035a53ab4ebdaf83355740dd7058
JessicaAKelley <kelleyajessica@gmail.com>
2021-08-14 12:17:19 -0500

    Testing to see if localhost error is fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,88 +3,88 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Card, User, Comment } = require("../models");
 
-// // Home Page - Get all Cards associated with user along with comments
+// Home Page - Get all Cards associated with user along with comments
 
-// //Get comments
-// router.get("/", (req, res) => {
-//   // Get all Comments
-//   Comment.findAll({
-//     attributes: ["id", "comment_text", "createdAt"],
-//     // Include card details
-//     include: [
-//       {
-//         model: Card,
-//         attributes: ["id", "user_id"],
-//         include: {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//       },
-//       // User who made comment
-//       {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     ],
-//   })
-//     .then((dbCommentData) => {
-//       // Upon success, get the comments and render it on homepage
-//       const comments = dbcommentData.map((comment) =>
-//         comment.get({ plain: true })
-//       );
-//       res.render("homepage", {
-//         comments,
-//         loggedIn: req.session.loggedIn,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+//Get comments
+router.get("/", (req, res) => {
+  // Get all Comments
+  Comment.findAll({
+    attributes: ["id", "comment_text", "createdAt"],
+    // Include card details
+    include: [
+      {
+        model: Card,
+        attributes: ["id", "user_id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      // User who made comment
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbCommentData) => {
+      // Upon success, get the comments and render it on homepage
+      const comments = dbCommentData.map((comment) =>
+        comment.get({ plain: true })
+      );
+      res.render("homepage", {
+        comments,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// // Render a single comment page
-// router.get("/comment/:id", (req, res) => {
-//   Comment.findOne({
-//     where: {
-//       id: req.params.id,
-//     },
-//     attributes: ["id", "createdAt", "comment_text"],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ["id", "card_id", "user_id"],
-//         include: {
-//           model: User,
-//           attributes: ["username"],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ["username"],
-//       },
-//     ],
-//   })
-//     .then((dbCommentData) => {
-//       if (!dbCommentData) {
-//         res.status(404).json({ message: "Not found " });
-//         return;
-//       }
+// Render a single comment page
+router.get("/comment/:id", (req, res) => {
+  Comment.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "createdAt", "comment_text"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "card_id", "user_id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "Not found " });
+        return;
+      }
 
-//       // serialize the data
-//       const comment = dbCommentData.get({ plain: true });
+      // serialize the data
+      const comment = dbCommentData.get({ plain: true });
 
-//       // pass data to template
-//       res.render("single-comment", {
-//         comment,
-//         loggedIn: req.session.loggedIn,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
+      // pass data to template
+      res.render("single-comment", {
+        comment,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // Login Page
 router.get("/login", (req, res) => {
