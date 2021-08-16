ee7d899c04518880fed2e8c8d6e30deccc5e5aa2
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-08-16 15:13:55 -0700

    updated card-routes and seeds to populate cards for front end

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-// getting the data for front end -  coding :Deepa 
-//dependencies   
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Card, User, Comment,Favorite } = require('../models');
-
-// Home Page - Get all Cards  associated with user along with comments 
- // to be filled - by Dyravuth yorn 
-=======
-//dependcies
+// getting the data for front end -  coding :Deepa
+//dependencies
 const router = require("express").Router();
 const sequelize = require("../config/connection");
-const axios = require("axios");
-const { Card, User, Comment } = require("../models");
->>>>>>> feature/cards
-
+const { Card, User, Comment, Favorite } = require("../models");
 // Home Page - Get all Cards  associated with user along with comments
-
+// to be filled - by Dyravuth yorn
 //Get comments
 router.get("/", (req, res) => {
   // Get all Comments
@@ -55,50 +43,20 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
-
 //Get comments
-router.get('/', (req, res) => {
-// Get all Comments 
-Comment.findAll({
-  attributes: [
-    'id',
-    'comment_text',
-    'user_id'
-  ],
-  // Include card details
-  include: [
-    {
-      model: Card,
-      attributes: ['id'],
-      include: {
-        model: User,
-        attributes: ['username']
-      }
-    },
-    // User who made comment  
-    {
-      model: User,
-      attributes: ['username']
-    }
-  ]
-})
-.then(dbCommentData =>{
-    // Upon success,Get the comments  and render it on homepage 
-    const comments = dbCommentData.map(comment => comment.get({ plain: true }));
-    res.render('homepage', {
-        comments,
-        loggedIn: req.session.loggedIn
-      });
-})
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-});
-});
-
-//Render a single comment  Page
-router.get('/comments/:id', (req, res) => {
-        attributes: ["api_id"],
+router.get("/", (req, res) => {
+  // Get all Comments
+  Comment.findAll({
+    attributes: ["id", "comment_text", "user_id"],
+    // Include card details
+    include: [
+      {
+        model: Card,
+        attributes: ["id"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
       },
       // User who made comment
       {
@@ -112,31 +70,9 @@ router.get('/comments/:id', (req, res) => {
       const comments = dbCommentData.map((comment) =>
         comment.get({ plain: true })
       );
-      Promise.all(
-        comments.map(async (comment) => {
-          comment.card.data = await new Promise((resolve, reject) => {
-            axios
-              .get(
-                "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/" +
-                  comment.card.api_id,
-                {
-                  headers: {
-                    "X-RapidAPI-Key": process.env.HS_APIKEY,
-                    "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-                  },
-                }
-              )
-              .then(({ data }) => resolve(data[0]))
-              .catch((error) => reject(error));
-          });
-          return comment;
-        })
-      ).then((comments) => {
-        console.log(comments);
-        res.render("homepage", {
-          comments,
-          loggedIn: req.session.loggedIn,
-        });
+      res.render("homepage", {
+        comments,
+        loggedIn: req.session.loggedIn,
       });
     })
     .catch((err) => {
@@ -144,9 +80,8 @@ router.get('/comments/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
 //Render a single comment  Page
-router.get("/comment/:id", (req, res) => {
+router.get("/comments/:id", (req, res) => {
   Comment.findOne({
     where: {
       id: req.params.id,
@@ -154,13 +89,8 @@ router.get("/comment/:id", (req, res) => {
     attributes: ["id", "createdAt", "comment_text"],
     include: [
       {
-<<<<<<< HEAD
         model: Card,
-        attributes: ['id', 'user_id'],
-=======
-        model: Comment,
-        attributes: ["id", "card_id", "user_id"],
->>>>>>> feature/cards
+        attributes: ["id", "user_id"],
         include: {
           model: User,
           attributes: ["username"],
@@ -177,10 +107,8 @@ router.get("/comment/:id", (req, res) => {
         res.status(404).json({ message: "Not found " });
         return;
       }
-
       // serialize the data
       const comment = dbCommentData.get({ plain: true });
-
       // pass data to template
       res.render("single-comment", {
         comment,
@@ -192,19 +120,14 @@ router.get("/comment/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
-
-<<<<<<< HEAD
-
 // Login Page
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
-
   res.render("login");
 });
-
 // Render Sign-up page
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
@@ -213,26 +136,4 @@ router.get("/signup", (req, res) => {
   }
   res.render("sign-up");
 });
-
-=======
-// Login  Page
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-
-  res.render("login");
-});
-
-// Render Sign up page
-router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("signup");
-});
-
->>>>>>> feature/cards
 module.exports = router;
--- seeds/card-seeds.js
+++ seeds/card-seeds.js
@@ -5,22 +5,37 @@ const carddata = [
   {
     user_id: 1,
     api_id: "HERO_02mbp",
+    card_img:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/cf014c0c9363f70cb7e4f7f7c4086ab42bc5d2afecf421f35b9c87c81943d9d4.png",
+    card_name: "Leeroy Jenkins",
   },
   {
     user_id: 1,
-    api_id: "CS2_188o",
+    api_id: "VAN_EX1_116",
+    card_img:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/e3a97bcb37b12fbe0845471ca0b3dc7deef2317f27544f55acccfd1a672ad1fd.png",
+    card_name: "Tank Up!",
   },
   {
     user_id: 2,
-    api_id: "EX1_014te",
+    api_id: "CFM_648t",
+    card_img:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/f8b2f6eef5466fb1fb0bed9cc06c667e032df2b847bc6b72fb1e3bca28b4c939.png",
+    card_name: "Little Friend",
   },
   {
     user_id: 3,
-    api_id: "EX1_604o",
+    api_id: "TRL_305",
+    card_img:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/1a76dd7384eb97550f52418c72a8aea5c9ce5a780826f417e006d17db7228fb8.png",
+    card_name: "A New Challenger",
   },
   {
     user_id: 4,
-    api_id: "EX1_549o",
+    api_id: "TB_BaconShop_HP_041",
+    card_img:
+      "https://d15f34w2p8l1cc.cloudfront.net/hearthstone/174b35548dcfed8a083d6a29f59b33d44d8c9661735affdfec5cac09b87a7b5a.png",
+    card_name: "A Tale of Kings",
   },
 ];
 const seedCards = () => Card.bulkCreate(carddata);
