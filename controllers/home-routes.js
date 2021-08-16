b641d9d44572ffd00ec2fa7f8ceeff36f78606f1
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-08-16 16:10:52 -0700

    starting on card-info.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -43,43 +43,6 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
-//Get comments
-router.get("/", (req, res) => {
-  // Get all Comments
-  Comment.findAll({
-    attributes: ["id", "comment_text", "user_id"],
-    // Include card details
-    include: [
-      {
-        model: Card,
-        attributes: ["id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      // User who made comment
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbCommentData) => {
-      // Upon success,Get the comments  and render it on homepage
-      const comments = dbCommentData.map((comment) =>
-        comment.get({ plain: true })
-      );
-      res.render("homepage", {
-        comments,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
 //Render a single comment  Page
 router.get("/comments/:id", (req, res) => {
   Comment.findOne({
@@ -135,5 +98,67 @@ router.get("/signup", (req, res) => {
     return;
   }
   res.render("sign-up");
+});
+
+// card section
+router.get("/", (req, res) => {
+  Card.findAll({
+    attributes: ["id", "api_id", "card_name", "card_img"],
+    include: [
+      // include the Card details here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  }).then((dbCardData) => {
+    // Upon success, get the cards data and render it on homepage
+    const cards = dbCardData.map((card) => card.get({ plain: true }));
+    res.render("homepage", {
+      cards,
+      loggedIn: req.session.loggedIn,
+    });
+  });
+});
+
+// card get by id
+router.get("/:id", (req, res) => {
+  Card.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "card_name", "api_id", "card_img"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((dbCardData) => {
+      if (!dbCardData) {
+        res.status(404).json({ message: "Not found " });
+        return;
+      }
+      // serialize the data
+      const card = dbCardData.get({ plain: true });
+      // pass data to template
+      res.render("single-card", {
+        card,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 module.exports = router;
