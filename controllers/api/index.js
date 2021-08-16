6e19ffc4c0664ad64351d4095e42698486d64871
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-08-16 13:54:36 -0700

    deleted double codes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,17 +7,6 @@ const userRoutes = require("./user-routes");
 const cardRoutes = require("./card-routes");
 const commentRoutes = require("./comment-routes");
 // const favRoutes = require("./favorite-routes");
-const router = require("express").Router();
-const sequelize = require("../../config/connection");
-const { User } = require("../../models");
-
-const userRoutes = require("./user-routes");
-const commentRoutes = require("./comment-routes");
-const cardRoutes = require("./card-routes");
-
-router.use("/users", userRoutes);
-router.use("/comments", commentRoutes);
-router.use("/cards", cardRoutes);
 
 router.use("/users", userRoutes);
 router.use("/cards", cardRoutes);
