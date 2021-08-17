b092c270e67bec0482828c1bffd86ed1a8db1a0b
JessicaAKelley <kelleyajessica@gmail.com>
2021-08-16 19:36:59 -0500

    API image

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -134,5 +134,66 @@ router.get('/create/', withAuth, (req, res) => {
       });
   });
 
+//   // card section
+// router.get("/", (req, res) => {
+//   Card.findAll({
+//     attributes: ["id", "api_id", "card_name", "card_img"],
+//     include: [
+//       // include the Card details here:
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//     ],
+//   }).then((dbCardData) => {
+//     // Upon success, get the cards data and render it on homepage
+//     const cards = dbCardData.map((card) => card.get({ plain: true }));
+//     res.render("homepage", {
+//       cards,
+//       loggedIn: req.session.loggedIn,
+//     });
+//   });
+// });
+
+// // card get by id
+// router.get("/:id", (req, res) => {
+//   Card.findOne({
+//     where: {
+//       id: req.params.id,
+//     },
+//     attributes: ["id", "card_name", "api_id", "card_img"],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//     ],
+//   })
+//     .then((dbCardData) => {
+//       if (!dbCardData) {
+//         res.status(404).json({ message: "Not found " });
+//         return;
+//       }
+//       // serialize the data
+//       const card = dbCardData.get({ plain: true });
+//       // pass data to template
+//       res.render("single-card", {
+//         card,
+//         loggedIn: req.session.loggedIn,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,164 +1,87 @@
-// getting the data for front end -  coding :Deepa
-//dependencies
-const router = require("express").Router();
-const sequelize = require("../config/connection");
-const { Card, User, Comment, Favorite } = require("../models");
-// Home Page - Get all Cards  associated with user along with comments
-// to be filled - by Dyravuth yorn
-//Get comments
-router.get("/", (req, res) => {
-  // Get all Comments
-  Comment.findAll({
-    attributes: ["id", "comment_text", "createdAt"],
-    // Include card details
-    include: [
-      {
-        model: Card,
-        attributes: ["id", "user_id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      // User who made comment
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbCommentData) => {
-      // Upon success, get the comments and render it on homepage
-      const comments = dbCommentData.map((comment) =>
-        comment.get({ plain: true })
-      );
-      console.log(comments);
-      res.render("homepage", {
-        comments,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-//Render a single comment  Page
-router.get("/comments/:id", (req, res) => {
-  Comment.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "createdAt", "comment_text"],
-    include: [
-      {
-        model: Card,
-        attributes: ["id", "user_id"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbCommentData) => {
-      if (!dbCommentData) {
-        res.status(404).json({ message: "Not found " });
-        return;
-      }
-      // serialize the data
-      const comment = dbCommentData.get({ plain: true });
-      // pass data to template
-      res.render("single-comment", {
-        comment,
-        loggedIn: req.session.loggedIn,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-// Login Page
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("login");
-});
-// Render Sign-up page
-router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("sign-up");
-});
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
+const withAuth = require('../utils/auth');
 
-// card section
-router.get("/", (req, res) => {
-  Card.findAll({
-    attributes: ["id", "api_id", "card_name", "card_img"],
-    include: [
-      // include the Card details here:
-      {
-        model: Comment,
-        attributes: ["id", "comment_text"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-    ],
-  }).then((dbCardData) => {
-    // Upon success, get the cards data and render it on homepage
-    const cards = dbCardData.map((card) => card.get({ plain: true }));
-    res.render("homepage", {
-      cards,
-      loggedIn: req.session.loggedIn,
-    });
-  });
-});
-
-// card get by id
-router.get("/:id", (req, res) => {
-  Card.findOne({
+// get all posts for dashboard
+router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
     where: {
-      id: req.params.id,
+      user_id: req.session.user_id
     },
-    attributes: ["id", "card_name", "api_id", "card_img"],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: Comment,
-        attributes: ["id", "comment_text"],
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
           model: User,
-          attributes: ["username"],
-        },
+          attributes: ['username']
+        }
       },
-    ],
-  })
-    .then((dbCardData) => {
-      if (!dbCardData) {
-        res.status(404).json({ message: "Not found " });
-        return;
+      {
+        model: User,
+        attributes: ['username']
       }
-      // serialize the data
-      const card = dbCardData.get({ plain: true });
-      // pass data to template
-      res.render("single-card", {
-        card,
-        loggedIn: req.session.loggedIn,
-      });
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
